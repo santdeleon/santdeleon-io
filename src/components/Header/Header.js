e68e4f58a6050d02c3b399a9fe579aa49865f810
Santiago Deleon <sant@santdeleon.co>
2019-04-14 20:05:39 -0500

    add global responsive styles and style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 import Navbar from '../Navbar/Navbar';
 
-
 function Header(props) {
+  const [roles] = useState(
+    [
+      {id: 1, role: 'Software Engineer', color: '#77EAC5'},
+      {id: 2, role: 'Web Developer', color: '#FED66A'},
+      {id: 3, role: 'Full-Stack Developer', color: '#2CC9E1'},
+      {id: 4, role: 'Blockchain Engineer', color: '#FC5AA4'},
+      {id: 5, role: 'Technology Enthusiast', color: '#BC7FFC'},
+      {id: 6, role: 'Coding Sensei', color: '#E14627'}
+    ]
+  );
+
+
   return (
-    <header id="header" className="header flex flex-column has-mobile-padding has-desktop-padding">
+    <header id="header" className="header flex flex-column">
       <Navbar logo={props.logo}/>
+
+      <section className="hero flex text-center">
+        <div className="hero-body flex flex-column">
+          <div className="hero-text flex flex-column">
+            <h1 className="title">Front-end Developer & Blockchain Engineer</h1>
+            <h2 className="subtitle text-muted">With a knack for creating elegantly smooth and stylish products.</h2>
+          </div>
+
+          <img src={require('../../assets/img/logo.svg')} className="avatar" alt="Cartoon Santiago"/>
+        </div>
+      </section>
+
+      <div className="roles flex">
+        {roles.map(role => {
+          return  <div key={role.id} className="role flex flex-column">
+                    <div className="role-top flex" style={{backgroundColor: role.color}}></div>
+                    <p className="text-muted">{role.role}</p>
+                  </div>
+        })}
+      </div>
     </header>
   );
 }
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route } from 'react-router';
 
 import Header from '../../components/Header/Header';
