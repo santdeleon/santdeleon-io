2d4209d0f4dc77ce8744052e49086050e8f7a056
Santiago Deleon <sant@santdeleon.co>
2019-04-17 20:06:36 -0500

    create skeleton for Skills section

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Skills.css';
+
+
+function Skills(props) {
+  return (
+    <section id="skills">
+      <div className="container flex">
+
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
