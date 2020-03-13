9f1feb84f490c5dd0524f245484974c9c1ec4171
Santiago Deleon <santiago.deleon@endurance.com>
2020-03-12 23:40:19 -0500

    Cleanup and refactor navbar code and css

--- src/components/NavLink/NavLink.js
+++ src/components/NavLink/NavLink.js
@@ -5,7 +5,7 @@ import './NavLink.css';
 function NavLink(props) {
 
   return (
-    <li className="nav-item fromLeft">
+    <li className="nav-link fromLeft">
       <a href={props.href} aria-label={props.name}>{props.name}</a>
     </li>
   );
