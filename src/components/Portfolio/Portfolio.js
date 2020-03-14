464d3a92c48885468c68140e2dc6394e1e220525
Santiago Deleon <santiago.deleon@endurance.com>
2020-03-14 11:58:31 -0500

    Finish Portfolio component styling and responsiveness

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -14,42 +14,25 @@ function Portfolio(props) {
       <div className="portfolio-wrapper flex flex-column align-items-center">
 
         <div className="project-row w-100 flex align-items-center justify-content-center">
-          <a id="hostgatorProjectCard" className="hostgator-project-card project flex align-items-center" href="#">
-              <div className="project-details flex flex-column align-items-start justify-content-between">
-                <div className="flex">
-                  <img className="project-logo" src={HostGator} alt="" />
-                </div>
-
-                <p className="project-text my-2 fw-bold">A platform for entrepeuners and businesses to create, host and manage websites</p>
-
-                <div className="project-footer flex-column align-items-center">
-                  <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
-                  <h4 className="project-industry m-0 fw-lighter">Domain & Web Hosting</h4>
-                </div>
-              </div>
-          </a>
-
-            <a id="martianProjectCard" className="martian-project-card project flex align-items-center" href="#">
+            <a id="hostgatorProjectCard" className="hostgator-project-card project flex align-items-center" href="#">
                 <div className="project-details flex flex-column align-items-start justify-content-between">
                   <div className="flex">
-                    <img className="project-logo" src={Martian} alt="" />
+                    <img className="project-logo" src={HostGator} width={120} height={50} alt="" />
                   </div>
-                  <h3 className="project-text my-2 fw-bold">Decentralized Blockchain solution for monetizing, distributing and hosting content</h3>
+
+                  <p className="project-text my-4 fw-bold">Platform for entrepeuners <br/> & businesses to create,<br/> host & manage websites</p>
 
                   <div className="project-footer flex-column align-items-center">
                     <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
-                    <h4 className="project-industry m-0 fw-lighter">Blockchain & Content Management</h4>
+                    <h4 className="project-industry m-0 fw-lighter">Domain & Web Hosting</h4>
                   </div>
                 </div>
             </a>
 
-        </div>
-
-        <div className="project-row w-100 flex align-items-center justify-content-center">
             <a id="terraProjectCard" className="terra-project-card project flex align-items-center" href="#">
                 <div className="project-details flex flex-column align-items-start justify-content-between">
-                  <img className="project-logo" src={Terra} width={100} alt="" />
-                  <h3 className="project-text my-2 fw-bold">A small in-house tech solution for custom software applications</h3>
+                  <img className="project-logo" src={Terra} width={90} height={50} alt="" />
+                  <p className="project-text my-4 fw-bold">Small in-house tech <br/> solution for custom <br/> software applications</p>
 
                   <div className="project-footer flex-column align-items-center">
                     <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
@@ -58,14 +41,31 @@ function Portfolio(props) {
                 </div>
             </a>
 
+        </div>
+
+        <div className="project-row w-100 flex align-items-center justify-content-center">
             <a id="homiesProjectCard" className="homies-project-card project flex align-items-center" href="#">
                 <div className="project-details flex flex-column align-items-start justify-content-between">
-                  <img className="project-logo" src={Homies} width={80} alt="" />
-                  <p className="project-text my-2 fw-bold">Grassroots startup aimed at helping brands get off the ground and sell online or rebrand their business</p>
+                  <img className="project-logo" src={Homies} width={70} height={50} alt="" />
+                  <p className="project-text my-4 fw-bold">Grassroots startup helping <br/> brands get off the ground <br/> & sell online</p>
 
                   <div className="project-footer flex-column align-items-center">
                     <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
                     <h4 className="project-industry m-0 fw-lighter">Branding & E-Commerce</h4>
+                  </div>
+                </div>
+            </a>
+
+            <a id="martianProjectCard" className="martian-project-card project flex align-items-center" href="#">
+                <div className="project-details flex flex-column align-items-start justify-content-between">
+                  <div className="flex">
+                    <img className="project-logo" src={Martian} width={120} height={50} alt="" />
+                  </div>
+                  <p className="project-text my-4 fw-bold">Blockchain solution for <br/> monetizing, distributing <br/> & hosting content</p>
+
+                  <div className="project-footer flex-column align-items-center">
+                    <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
+                    <h4 className="project-industry m-0 fw-lighter">Blockchain & Content Management</h4>
                   </div>
                 </div>
             </a>
