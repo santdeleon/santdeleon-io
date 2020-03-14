46b951791039de76770276d2bd604a84d6764055
Santiago Deleon <santiago.deleon@endurance.com>
2020-03-14 14:56:29 -0500

    Create ProjectCard component and refactor Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import ProjectCard from '../ProjectCard/ProjectCard';
+
 import HostGator from '../../assets/img/hostgator-logo.svg';
 import Martian from '../../assets/img/martian-text-logo.svg';
 import Homies from '../../assets/img/homies-logo.png';
@@ -7,70 +9,69 @@ import Terra from '../../assets/img/terra-ats-logo.png';
 
 import './Portfolio.css';
 
+
 function Portfolio(props) {
+  const projects = {
+    hostgator: {
+      id: 'hostgatorProjectCard', class: 'hostgator-project-card', url: '#', img: HostGator,
+      width: 120, height: 50, text: 'Platform for entrepeuners <br /> & businesses to create,<br /> host & manage websites', industry: 'Domain & Web Hosting'
+    },
+    terra: {
+      id: 'terraProjectCard', class: 'terra-project-card', url: '#', img: Terra,
+      width: 90, height: 50, text: 'Small in-house tech <br /> solution for custom <br /> software applications', industry: 'Software Services & IT Solutions'
+    },
+    homies: {
+      id: 'homiesProjectCard', class: 'homies-project-card', url: '#', img: Homies,
+      width: 70, height: 50, text: 'Grassroots startup helping <br /> brands get off the ground <br /> & sell online', industry: 'Branding & E-Commerce'
+    },
+    martian: {
+      id: 'martianProjectCard', class: 'martian-project-card', url: '#', img: Martian,
+      width: 120, height: 50, text: 'Blockchain solution for <br /> monetizing, distributing <br /> & hosting content', industry: 'Blockchain & Content Management'
+    }
+  }
 
   return (
     <div id="Portfolio" className="Portfolio container">
       <div className="portfolio-wrapper flex flex-column align-items-center">
 
+
         <div className="project-row w-100 flex align-items-center justify-content-center">
-            <a id="hostgatorProjectCard" className="hostgator-project-card project flex align-items-center" href="#">
-                <div className="project-details flex flex-column align-items-start justify-content-between">
-                  <div className="flex">
-                    <img className="project-logo" src={HostGator} width={120} height={50} alt="" />
-                  </div>
-
-                  <p className="project-text my-4 fw-bold">Platform for entrepeuners <br/> & businesses to create,<br/> host & manage websites</p>
-
-                  <div className="project-footer flex-column align-items-center">
-                    <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
-                    <h4 className="project-industry m-0 fw-lighter">Domain & Web Hosting</h4>
-                  </div>
-                </div>
-            </a>
-
-            <a id="terraProjectCard" className="terra-project-card project flex align-items-center" href="#">
-                <div className="project-details flex flex-column align-items-start justify-content-between">
-                  <img className="project-logo" src={Terra} width={90} height={50} alt="" />
-                  <p className="project-text my-4 fw-bold">Small in-house tech <br/> solution for custom <br/> software applications</p>
-
-                  <div className="project-footer flex-column align-items-center">
-                    <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
-                    <h4 className="project-industry m-0 fw-lighter">Software Services & IT Solutions</h4>
-                  </div>
-                </div>
-            </a>
-
+          {Object.keys(projects).map((project, index) => {
+            if (index < 2) {
+              return  <ProjectCard
+                        name={project}
+                        key={projects[project].id}
+                        id={projects[project].id}
+                        class={projects[project].class}
+                        url={projects[project].url}
+                        img={projects[project].img}
+                        width={projects[project].width}
+                        height={projects[project].height}
+                        text={projects[project].text}
+                        industry={projects[project].industry}
+                      />
+            }
+          })}
         </div>
 
         <div className="project-row w-100 flex align-items-center justify-content-center">
-            <a id="homiesProjectCard" className="homies-project-card project flex align-items-center" href="#">
-                <div className="project-details flex flex-column align-items-start justify-content-between">
-                  <img className="project-logo" src={Homies} width={70} height={50} alt="" />
-                  <p className="project-text my-4 fw-bold">Grassroots startup helping <br/> brands get off the ground <br/> & sell online</p>
-
-                  <div className="project-footer flex-column align-items-center">
-                    <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
-                    <h4 className="project-industry m-0 fw-lighter">Branding & E-Commerce</h4>
-                  </div>
-                </div>
-            </a>
-
-            <a id="martianProjectCard" className="martian-project-card project flex align-items-center" href="#">
-                <div className="project-details flex flex-column align-items-start justify-content-between">
-                  <div className="flex">
-                    <img className="project-logo" src={Martian} width={120} height={50} alt="" />
-                  </div>
-                  <p className="project-text my-4 fw-bold">Blockchain solution for <br/> monetizing, distributing <br/> & hosting content</p>
-
-                  <div className="project-footer flex-column align-items-center">
-                    <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
-                    <h4 className="project-industry m-0 fw-lighter">Blockchain & Content Management</h4>
-                  </div>
-                </div>
-            </a>
+          {Object.keys(projects).map((project, index) => {
+            if (index >= 2) {
+              return  <ProjectCard
+                        name={project}
+                        key={projects[project].id}
+                        id={projects[project].id}
+                        class={projects[project].class}
+                        url={projects[project].url}
+                        img={projects[project].img}
+                        width={projects[project].width}
+                        height={projects[project].height}
+                        text={projects[project].text}
+                        industry={projects[project].industry}
+                      />
+            }
+          })}
         </div>
-
       </div>
     </div>
   );
---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './ProjectCard.css';
+
+
+function ProjectCard(props) {
+
+  return (
+    <a id={props.id} className={`${props.class} project flex align-items-center`} href={props.url}>
+        <div className="project-details flex flex-column align-items-start justify-content-between">
+          <div className="flex">
+            <img className="project-logo" src={props.img} width={props.width} height={props.height} alt={props.name} />
+          </div>
+
+          <p className="project-text my-4 fw-bold" dangerouslySetInnerHTML={{__html: props.text}}></p>
+
+          <div className="project-footer flex-column align-items-center">
+            <h4 className="project-industry-title mb-1 fw-bold">Industry</h4>
+            <h4 className="project-industry m-0 fw-lighter">{props.industry}</h4>
+          </div>
+        </div>
+    </a>
+  );
+}
+
+export default ProjectCard;
