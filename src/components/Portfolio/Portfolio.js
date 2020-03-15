94bda520701de568baf0d0a1b00b48fbea664741
Santiago Deleon <santiago.deleon@endurance.com>
2020-03-14 21:30:56 -0500

    Add Coinflip project to portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,6 +6,7 @@ import HostGator from '../../assets/img/hostgator-logo.svg';
 import Martian from '../../assets/img/martian-text-logo.svg';
 import Homies from '../../assets/img/homies-logo.png';
 import Terra from '../../assets/img/terra-ats-logo.png';
+import Ethereum from '../../assets/img/ethereum-logo.png';
 
 import './Portfolio.css';
 
@@ -27,6 +28,10 @@ function Portfolio(props) {
     martian: {
       id: 'martianProjectCard', class: 'martian-project-card', url: '#', img: Martian,
       width: 120, height: 50, text: 'Blockchain solution for <br /> monetizing, distributing <br /> & hosting content', industry: 'Blockchain & Content Management'
+    },
+    coinflip: {
+      id: 'coinflipProjectCard', class: 'coinflip-project-card', url: '#', img: Ethereum,
+      width: 40, height: 40, text: 'Decentralized gambling <br /> application built on the <br /> Ethereum Network', industry: 'Blockchain, Gambling & Gaming'
     }
   }
 
@@ -54,7 +59,7 @@ function Portfolio(props) {
 
         <div className="project-row w-100 flex align-items-center justify-content-center">
           {Object.keys(projects).map((project, index) => {
-            if (index >= 2) {
+            if (index >= 2 && index < 4) {
               return  <ProjectCard
                         name={project}
                         key={projects[project].id}
@@ -69,6 +74,21 @@ function Portfolio(props) {
                       />
             }
           })}
+        </div>
+
+        <div className="project-row w-100 flex align-items-center justify-content-center">
+          <ProjectCard
+            name={projects['coinflip'].name}
+            key={projects['coinflip'].id}
+            id={projects['coinflip'].id}
+            class={projects['coinflip'].class}
+            url={projects['coinflip'].url}
+            img={projects['coinflip'].img}
+            width={projects['coinflip'].width}
+            height={projects['coinflip'].height}
+            text={projects['coinflip'].text}
+            industry={projects['coinflip'].industry}
+          />
         </div>
       </div>
     </div>
