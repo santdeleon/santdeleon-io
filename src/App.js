8688401a84b0d2a7ba7b2e671d74f5ae50d9b446
Sant Deleon <sant@santdeleon.io>
2020-12-02 19:45:01 -0600

    style: pause Particles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Particles from "particlesjs";
+// import Particles from "particlesjs";
 
 import Layout from "./components/Layout";
 
@@ -8,28 +8,28 @@ import { GlobalStyles, useTheme } from "./context/useTheme";
 import "./App.css";
 
 const App = () => {
-  const { theme } = useTheme();
+  // const { theme } = useTheme();
 
   window.onload = () =>
     console.log("%c SANT WUZ HERE! ", "background: #222; color: #bada55");
 
-  useEffect(() => {
-    const particles = Particles.init({
-      selector: ".App__Particles",
-      color: theme === "light" ? "#222" : "#fff",
-      speed: 0.3,
-      sizeVariations: 4
-    });
-
-    // reset Particles on cleanup to prevent particle re-renders
-    return () => {
-      particles.pauseAnimation();
-    };
-  }, [theme]);
+  // useEffect(() => {
+  //   const particles = Particles.init({
+  //     selector: ".App__Particles",
+  //     color: theme === "light" ? "#222" : "#fff",
+  //     speed: 0.3,
+  //     sizeVariations: 4
+  //   });
+  //
+  //   // reset Particles on cleanup to prevent particle re-renders
+  //   return () => {
+  //     particles.pauseAnimation();
+  //   };
+  // }, [theme]);
 
   return (
     <div className="App">
-      <canvas className="App__Particles" />
+      {/* <canvas className="App__Particles" /> */}
       <div className="App__Div--rainbow-top" />
       <GlobalStyles />
       <Layout />
