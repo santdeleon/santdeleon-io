766295b17d04c7913b81f8b6de1841d2180c9467
Sant Deleon <sant@santdeleon.io>
2020-12-25 00:51:24 -0600

    test: write simple snapshot test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import { ThemeProvider } from './context/ThemeContext';
+import { BrandProvider } from './context/BrandContext';
 
 import App from './App';
 
+// TODO: Get the portfolio test coverage up to 100%
 describe('the App component', () => {
-  it('renders with a welcome message', () => {
-    render(
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>,
+  it('should match snapshot', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <BrandProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </BrandProvider>
+      </BrowserRouter>,
     );
-    const header = screen.getByRole('heading', { name: 'Hello, World!' });
-    expect(header).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 });
