8e93c42590341343733db893d4329086f8ad1450
Sant Deleon <sant@santdeleon.io>
2021-03-27 14:51:57 -0500

    docs(projects): add design system to projects directory

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,21 @@
 export const projects = [
   {
     id: 0,
-    title: 'santdeleon.io',
+    title: 'Liquidrainbowpony Design System v1',
     date: '03-27-2021',
-    href: 'https://github.com/santdeleon/santdeleon-io',
+    href: 'https://github.com/santdeleon/liquidrainbowpony-design-system',
   },
+
   {
     id: 1,
     title: 'Coinflip',
     date: '01-07-2021',
     href: 'https://github.com/santdeleon/coinflip',
+  },
+  {
+    id: 2,
+    title: 'santdeleon.io',
+    date: '03-27-2021',
+    href: 'https://github.com/santdeleon/santdeleon-io',
   },
 ];
